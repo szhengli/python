672c49b8aea900d95aa3b9d104767874d7695834
james <opre@163.com>
2019-05-06 09:24:59 +0800

    Add Concurrent. Docker files

--- Python_learn/class_test.py
+++ Python_learn/class_test.py
@@ -1,35 +1,15 @@
-from multiprocessing import Process, Lock, Queue,JoinableQueue
-from random import random, choice
-import random
-import  os, time, json
-from hashlib import md5
+glb = "gloal"
 
+def maker(n):
+    loc="local"
+    def action(x):
+        inside = "inside"
+        print("%s from global " +
+              "%s from n  " +
+              "%s from local " +
+              "%s from x " +
+              "%s from inside" % (glb, n, loc,x, inside ))
+    return action
 
-def consumer(q,name):
-    while True:
-        res = q.get()
-        time.sleep(random.randint(1,3))
-        print("%s eat %s" % (name, res))
-        q.task_done()
-
-def producer(q, name, food):
-    for i in range(3):
-        time.sleep(random.randint(1,3))
-        res = "%s %s" % (food, i)
-        q.put(res)
-        print("%s produces %s" % (name, res))
-    q.join()
-
-if __name__=="__main__":
-    q =JoinableQueue()
-    p1 = Process(target=producer, args=(q,'james','apple'))
-    c1 = Process(target=consumer, args=(q,'dog'))
-    c1.daemon =  True
-    p1.start()
-    c1.start()
-    p1.join()
-
-
-    print('end!')
-
+m=maker("james-n")
 
